9ce7d5f574c34f201539073e6efbc08d2aead3c0
wennyr <wennyramadha@gmail.com>
2021-08-13 13:54:32 +0800

    Add the grayscale process

--- open_camera.py
+++ open_camera.py
@@ -2,29 +2,9 @@
 
 
 import numpy as np
-# import cv2
-#
-# cap = cv2.VideoCapture(1)
-# # cap = cv2.VideoCapture('https://140.115.141.162:8080')
-# while(True):
-#     # Capture frame-by-frame
-#     ret, frame = cap.read()
-#
-#     # Our operations on the frame come here
-#     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-#
-#     # Display the resulting frame
-#     cv2.imshow('frame',gray)
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
-#         break
-#
-# # When everything done, release the capture
-# cap.release()
-# cv2.destroyAllWindows()
-
-import numpy as np
 import cv2 as cv
 cap = cv.VideoCapture(0)
+# cap = cv2.VideoCapture('https://140.115.141.162:8080')
 if not cap.isOpened():
     print("Cannot open camera")
     exit()
